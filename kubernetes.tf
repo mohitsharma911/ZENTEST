3dfe26627223285006e8636ccb8f278ad34192d3
mohitsharma911 <49859433+mohitsharma911@users.noreply.github.com>
2022-02-28 14:19:22 +0100

    Add files via upload

---
+++ kubernetes.tf
@@ -0,0 +1,21 @@
+provider "kubernetes" {
+    host = data.aws_eks_cluster.cluster.endpoint
+    token = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
+}
+provider "kubectl" {
+  host                   = var.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(var.eks_cluster_ca)
+  token                  = data.aws_eks_cluster_auth.main.token
+  load_config_file       = false
+}
---
+++ vars.tf
@@ -0,0 +1,6 @@
+variable "access_key" {
+  default = "AKIA3GHHOVOYCMMTMI4D"
+}
+variable "secret_key" {
+    default = "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"
+}
